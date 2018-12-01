4aba961184048403647b45dd1ae2e4f6c356fee9
sukm <sung.kim572@gmail.com>
2018-11-30 22:33:27 -0800

    Deploying

--- server/index.js
+++ server/index.js
@@ -6,12 +6,15 @@ const path = require('path');
 
 const PORT = 3005;
 const app = express();
+
+app.use('/courses/:courseId', express.static(path.join(__dirname, '/../public')));
+
+// app.get('/courses/:courseId/header', proxy('http://ec2-34-201-82-146.compute-1.amazonaws.com:3003/'));
+app.get('/courses/:courseId/recommendedCourses', proxy('http://ec2-52-53-187-75.us-west-1.compute.amazonaws.com:3008/'));
+// app.get('/:Id/instructors', proxy('http://54.183.145.251/'));
+// app.get('/:courseId/reviews', proxy('http://ec2-35-153-179-188.compute-1.amazonaws.com:3002/'));
+
+
 app.listen(PORT, () => {
   console.log(`listening on http://localhost:${PORT}/`);
 });
-app.use('/courses/:courseId', express.static(path.join(__dirname, '/../public')));
-
-app.get('/courses/:courseId/header', proxy('http://ec2-34-201-82-146.compute-1.amazonaws.com:3003/'));
-app.get('/courses/:courseId/recommendedCourses', proxy('http://ec2-52-53-187-75.us-west-1.compute.amazonaws.com:3008/'));
-app.get('/:Id/instructors', proxy('http://54.183.145.251/'));
-app.get('/:courseId/reviews', proxy('http://ec2-35-153-179-188.compute-1.amazonaws.com:3002/'));
